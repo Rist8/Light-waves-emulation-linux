817b43c71ac290a0c70f08d315154a689f1d8653
Rist8 <94626383+Rist8@users.noreply.github.com>
2023-12-13 22:20:03 +0100

    Update Wave_based_graphics.cpp
    updated version

--- Wave_based_graphics.cpp
+++ Wave_based_graphics.cpp
@@ -1,33 +1,26 @@
-﻿#include <stdio.h>
 #include <math.h>
-#include <functional>
-#include <conio.h>
 #include <windows.h>
 #include <iostream>
-#include <thread>
 
 class Block {
 public:
-	Block():weight(1),speed(0),height(0){}
-	Block(double w, double h):weight(w),speed(0),height(h){}
-	void Update() {
-		height += speed;
-	}
-	void set_weight(double w) { weight = w; }
-	double get_weight() { return weight; }
-	void set_height(double h) { height = h; }
-	double get_height() { return height; }
-	void add_speed(double s) { speed += s; }
-	double get_speed() { return speed; }
-private:
+	Block(double w = 1, double h = 0):weight(w),speed(0),height(h){}
+	void Update() { height += speed; }
+	
 	double weight, speed, height;
+
 };
 
 class Wave {
 public:
-	Wave() :width(50), height(50) { Init(); }
-	Wave(unsigned w, unsigned h) :width(w), height(h) { Init(); }
-	~Wave() { delete[] wave; }
+	Wave(unsigned w = 50, unsigned h = 50) :width(w), height(h) { Init(); }
+
+	~Wave() { 
+		for (int i = 0; i < width; ++i)
+			delete[] wave[i];
+		delete[] wave; 
+	}
+
 	void Update() {
 		for (int i = 1; i < width - 1; ++i)
 			for (int j = 1; j < height - 1; ++j)
@@ -36,36 +29,72 @@ public:
 			for (int j = 1; j < height - 1; ++j)
 				Block_Update(i, j);
 	}
-	void Circle(int x, int y, int r) {
+
+	void ImpulseCircle(int x, int y, int r) {
 		for (int i = x - r; i <= x + r; ++i)
 			for (int j = y - r; j <= y + r; ++j)
-				if (pow(i - x,2) + pow(j - y, 2) <= r * r) wave[i][j].set_height(1 - ((pow(i - x, 2) + pow(j - y, 2)) / (r * r)));
+				if (pow(i - x,2) + pow(j - y, 2) <= r * r)
+					wave[i][j].height=(1 - ((pow(i - x, 2) + pow(j - y, 2)) / (r * r)));
 	}
-	void Dot(int x, int y) {
-		wave[x][y].set_height(10);
+
+	void ImpulseRectangle(int x, int y, int a, int b) {
+		for (int i = x; i < x + a; ++i)
+			for (int j = y; j < y + b; ++j)
+				if (i == x || j == y || i == x + a - 1 || j == y + b - 1)
+					wave[i][j].height=(1);
 	}
-	double operator()(int x, int y) { return wave[x][y].get_height(); }
+
+	void ObjectFilledCircle(int x, int y, int r) {
+		for (int i = x - r; i <= x + r; ++i)
+			for (int j = y - r; j <= y + r; ++j)
+				if (pow(i - x,2) + pow(j - y, 2) <= r * r)
+					wave[i][j].weight=(9999);
+	}
+
+	void ObjectFilledRectangle(int x, int y, int a, int b) {
+		for (int i = x; i < x + a; ++i)
+			for (int j = y; j < y + b; ++j)
+				wave[i][j].weight=(9999);
+	}
+
+	void ObjectRectangle(int x, int y, int a, int b) {
+		for (int i = x; i < x + a; ++i)
+			for (int j = y; j < y + b; ++j)
+				if(i == x || j == y || i == x + a - 1 || j == y + b - 1)
+					wave[i][j].weight=(9999);
+	}
+
+	void ImpulseDot(int x, int y) {
+		wave[x][y].height=(10);
+	}
+
+	double operator()(int x, int y) { return wave[x][y].height; }
+	double operator()(int x, int y, bool weight) { return wave[x][y].weight; }
 private:
 	void Init() {
 		wave = new Block*[width + 2];
 		for (int i = 0; i < width; ++i)
 			wave[i] = new Block[height + 2];
 		for (int j = 0; j < height; ++j)
-			wave[0][j].set_weight(9999), wave[width - 1][j].set_weight(9999);
+			wave[0][j].weight=(9999), wave[width - 1][j].weight=(9999);
 		for (int j = 0; j < width; ++j)
-			wave[j][0].set_weight(9999), wave[j][height - 1].set_weight(9999);
+			wave[j][0].weight=(9999), wave[j][height - 1].weight=(9999);
 	}
+
 	void Block_UpdateSpeed(int x, int y) {
-		double left = wave[x - 1][y].get_height();
-		double up = wave[x][y - 1].get_height();
-		double right = wave[x + 1][y].get_height();
-		double down = wave[x][y + 1].get_height();
-		double sum = (left + down + right + up);
-		wave[x][y].add_speed((sum / 4 - wave[x][y].get_height()) / wave[x][y].get_weight());
+		static double left, up, right, down, sum;
+		left = wave[x - 1][y].height;
+		up = wave[x][y - 1].height;
+		right = wave[x + 1][y].height;
+		down = wave[x][y + 1].height;
+		sum = (left + down + right + up);
+		wave[x][y].speed+=((sum / 4 - wave[x][y].height) / wave[x][y].weight);
 	}
+
 	void Block_Update(int x, int y) {
 		wave[x][y].Update();
 	}
+
 	Block** wave;
 	unsigned width, height;
 };
@@ -94,29 +123,32 @@ int main() {
 	aspect *= pixelAspect;
 	char gradient[] = " .:!/r(l1Z4H9W8$@";
 	char invgradient[] = "@$8W9H4Z1l(r/!:. ";
-	int gradientSize = std::size(gradient) - 2;
+	int gradientSize = std::size(gradient);
 
 	wchar_t* screen = new wchar_t[width * height];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
 	Wave map(width,height);
-	map.Circle(100, 30, 10);
-	map.Circle(140, 30, 10);
-	int frames = 0;
+	map.ImpulseCircle(100, 30, 10);
+	map.ImpulseCircle(140, 30, 10);
+	//map.ImpulseDot(50, 20);
+	//map.ImpulseDot(50, 30);
 	while (1) {
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				int index = map(i, j) / 0.1;
-				char pixel = gradient[min(max(index + 2, 1), 16)];
+				char pixel = gradient[min(max(abs(index), 0), 16)];
+				if (map(i, j, 1) == 9999)
+					pixel = '*';
 				screen[i + j * width] = pixel;
 			}
 		}
 		screen[width * height - 1] = '\0';
 		WriteConsoleOutputCharacterW(hConsole, screen, width * height, { 0, 0 }, &dwBytesWritten);
 		map.Update();
-		Sleep(10);
-		++frames;
+		//Sleep(10);
 	}
 }
